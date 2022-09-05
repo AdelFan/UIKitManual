416e8f358a6ce51fef694e0348a3797d518a0033
Drdev <fallen.bmxer@gmail.com>
2022-09-05 16:44:13 +0500

    Create SourceHTML and TabBarController.

---
+++ UIKitManual/UIKitManual/Resources/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  UIKitManual
+//
+//  Created by Адель Ахметшин on 05.09.2022.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
