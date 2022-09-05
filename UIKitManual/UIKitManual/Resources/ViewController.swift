77e9dc3bda525a2641b62ace0bcbaa82b13d61f2
Drdev <fallen.bmxer@gmail.com>
2022-09-05 16:09:21 +0500

    Structure setup.

---
+++ UIKitManual/UIKitManual/Resources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitManual
+//
+//  Created by Адель Ахметшин on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
